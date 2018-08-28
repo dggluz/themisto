5543c6fa23c22adda44d832f940c2d310afff339
Gonza Gluzman <dggluz@gmail.com>
2018-08-28 11:54:23 -0300

    refactor(general): moved function to another file

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,10 @@
 import { getQueryResults, getProductInformation } from './providers/easy.provider';
-import { Task, UncaughtError } from '@ts-task/task';
-import { share } from '@ts-task/utils';
 import { closeBrowser } from './puppeteer-utils';
-import { tapChain, tap } from './utils';
-
-const limitConcurrency = <A, T, E> (maxConcurrentTasks: number, toTaskFn: (x: A) => Task<T, E>) =>
-	function processTasks (input: A[]) {
-		console.log(`Have to process ${input.length} tasks in chunks of ${maxConcurrentTasks}`);
-		return new Task<T[], E | UncaughtError>((resolve, reject) => {
-			Task
-				.all(input
-					.slice(0, maxConcurrentTasks)
-					.map(toTaskFn)
-				)
-				.chain(previousResults =>
-					input.length <= maxConcurrentTasks ?
-						Task.resolve(previousResults) :
-						processTasks(input.slice(maxConcurrentTasks))
-							.map(tailResults => previousResults.concat(tailResults))
-				)
-				.fork(reject, resolve)
-			;
-		});
-	}
-;
+import { tapChain, tap, limitConcurrency } from './utils';
 
 // TODO: try with "silla"
 getQueryResults('heladera')
 	.map(tap(x => console.log('Obtenidos los resultados!', x)))
-	.pipe(share())
 	.chain(limitConcurrency(3, getProductInformation))
 	.map(tap(x => console.log('Products information', x)))
 	.chain(tapChain(closeBrowser))
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { Task } from '@ts-task/task';
+import { Task, UncaughtError } from '@ts-task/task';
 
 export const tapChain = <A, B, E> (fn: (x: A) => Task<B, E>) =>
 	(x: A) =>
@@ -10,5 +10,26 @@ export const tap = <A> (fn: (x: A) => any) =>
 	(x: A)	=> {
 		fn(x);
 		return x;
+	}
+;
+
+export const limitConcurrency = <A, T, E> (maxConcurrentTasks: number, toTaskFn: (x: A) => Task<T, E>) =>
+	function processTasks (input: A[]) {
+		console.log(`Have to process ${input.length} tasks in chunks of ${maxConcurrentTasks}`);
+		return new Task<T[], E | UncaughtError>((resolve, reject) => {
+			Task
+				.all(input
+					.slice(0, maxConcurrentTasks)
+					.map(toTaskFn)
+				)
+				.chain(previousResults =>
+					input.length <= maxConcurrentTasks ?
+						Task.resolve(previousResults) :
+						processTasks(input.slice(maxConcurrentTasks))
+							.map(tailResults => previousResults.concat(tailResults))
+				)
+				.fork(reject, resolve)
+			;
+		});
 	}
 ;
